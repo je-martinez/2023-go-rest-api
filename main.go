1c16b75a5ad5e4bebf3d0e6ac91e55912467b01e
Jose E. Martinez <josemartinezchevez@gmail.com>
2023-05-13 12:12:23 -0600

    fix: remove old redis client factor

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/je-martinez/2023-go-rest-api/config"
-	"github.com/je-martinez/2023-go-rest-api/pkg/cache"
 	"github.com/je-martinez/2023-go-rest-api/pkg/logger"
 	"github.com/je-martinez/2023-go-rest-api/pkg/server"
 )
@@ -12,6 +11,5 @@ func main() {
 	appLogger := logger.NewApiLogger(config)
 	appLogger.InitLogger()
 	appLogger.Infof("AppVersion: %s, LogLevel: %s, Mode: %s, SSL: %v", config.Server.AppVersion, config.Logger.Level, config.Server.Mode, config.Server.SSL)
-	cache.InitRedisClient(config)
 	server.Start(config)
 }
